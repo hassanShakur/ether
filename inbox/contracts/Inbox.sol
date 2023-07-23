b8359cc7f7ad1086b85983cfe9f7abf7ba003e6e
hassanShakur <hass.shakurhassan@gmail.com>
2023-07-23 14:33:13 +0300

    truffle init

--- inbox/contracts/Inbox.sol
+++ inbox/contracts/Inbox.sol
@@ -1,7 +1,14 @@
-pragma solidity ^0.4.17
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
 
 contract Inbox {
-    function name(type name) {
-        
+    string public msg;
+
+    function Inbox(string initMsg) public {
+        msg = initMsg
+    }
+
+    function set(string newMsg) public {
+        msg = newMsg
     }
 }
